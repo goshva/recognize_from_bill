b900a1db1f100fb7d271b76759318297b1c902c9
adcurse22 <orhan03233@gmail.com>
2023-10-08 16:02:20 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import easyocr
+def text_recognize(file_path):
+    reader = easyocr.Reader(["ru"])
+    result = reader.readtext(file_path, detail=0, allowlist='1,2,3,4,5,6,7,8,9,0')
+
+    # Проверяем, что есть как минимум два элемента в результате
+    if len(result) >= 3:
+        # Второй элемент (индекс 1) и третий элемент (индекс 2)
+        print(result[2])  # Второй элемент
+        print(result[3])  # Третий элемент
+    # Проверяем равен ли вывод 2 номера и 3 если нето то это не купюра
+    elif result[2] == result[3]:
+        print("It's not a bill")
+    else:
+        print("Not enough text elements found in the image.")
+
+    return result
+
+
+def main():
+    file_path = input("Enter a file path: ")
+    print(text_recognize(file_path=file_path))
+
+
+if __name__ == "__main__":
+    main()
